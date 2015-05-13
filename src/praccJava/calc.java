97454fd2dd7f8eec1582c52aa8a0089f5e1efbe8
Zhluktenko Dmytro <d1mnewz@gmail.com>
2015-05-13 16:48:24 +0300

    added comments & fixed few things

--- src/praccJava/calc.java
+++ src/praccJava/calc.java
@@ -1,48 +1,47 @@
 package praccJava;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
 
 
 public class calc {
 
 	public calc() {
-		//System.out.println("hello calc");
+		
 		System.out.println("Calculator via Java v0.0.1");
 	}
 	private double digit1;
 	private double digit2;
 	private double res;
-	private Scanner _in;
+	private Scanner _in; 
 	
 
-	public void Calculate(String str) throws IllegalArgumentException
+	public void Calculate(String str) throws IllegalArgumentException // in case of divide by zero
 	{
 		if(str.equals("0") || str.isEmpty() || Integer.parseInt(str) > 4)
 		{
-			System.exit(0);
+			System.exit(0); // in case if input for menu is invalid
 		}
-		try {
-			this.GetDigits();
-		} catch (IOException e) {
+		
+		try 
+		{
+			this.GetDigits(); // if input for menu is valid -> get two digits for operation
+		} 
+		catch (IOException e) 
+		{
 			
-			System.out.println("Invalid input");
+			System.out.println("Invalid input"); // if digits are invalid or smth
 		}
 			switch(str)
 			{
 
 			case "1":
-				//System.out.println("1 pressed");
-				
 				this.res = this.digit1 + this.digit2;
 				System.out.println("result of " + digit1 + " + " + digit2 + " = " + this.res);
-				//JOptionPane.showMessageDialog(null, (int)this.res, "InfoBox: " + "Result", JOptionPane.INFORMATION_MESSAGE);
 				break;
 				
 			case "2":
@@ -59,15 +58,15 @@ public class calc {
 			case "4":
 				if(this.digit2 == 0)
 				{
-					throw new IllegalArgumentException("Argument 'digit2' is 0");
+					throw new IllegalArgumentException("Argument 'digit2' is 0"); // in case of divide by zero we -> 
+																			      //throw exception that is being handled in Menu()
 				}
 				this.res = this.digit1 / this.digit2;
 				System.out.println("result of " + digit1 + " / " + digit2 + " = " + this.res);
-				
 				break;
 				
 			default:
-				System.out.println("go away idiot =)");
+				System.out.println("invalid input."); 
 				break;
 			
 			}
@@ -75,23 +74,25 @@ public class calc {
 	
 	public void Menu()
 	{	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//Scanner scan = new Scanner(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // use this to read choice from keyboard
 		System.out.println("\tMenu\t");
 		System.out.println("1. plus.");
 		System.out.println("2. minus");
 		System.out.println("3. multiply.");
 		System.out.println("4. divide");
 		System.out.println("0. exit");
-		String choice = null;
-		try {
-			choice = br.readLine();
-		} catch (IOException e1) {
-			JOptionPane.showMessageDialog(null, e1.getMessage(), "InfoBox: " + "Exception!", JOptionPane.INFORMATION_MESSAGE);
+		String choice = null; // null`ing String so we can access it
+		try 
+		{
+			choice = br.readLine(); 
+		} 
+		catch (IOException e1) 
+		{
+			System.out.println(e1.getMessage());  // if our choice isnt parseable
 		}
 		try
 		{
-		this.Calculate(choice);
+			this.Calculate(choice);
 		}
 		catch (IllegalArgumentException ex)
 		{
@@ -99,9 +100,11 @@ public class calc {
 		}
 		
 		System.out.println("press any key to exit...");
-		try {
+		try 
+		{
 			System.in.read();
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			e.getMessage();
 		}
 		System.exit(0);
@@ -111,18 +114,19 @@ public class calc {
 	
 	public void GetDigits() throws IOException
 	{
-		System.out.println("Digit 1 :");
+		
 		_in = new Scanner(System.in);
+		System.out.println("Digit 1 :");
 		try
 		{
 			this.digit1 = Double.parseDouble(_in.next());
 		}
 		catch (InputMismatchException ex)
 		{	
-			System.out.println("wtf u entered m8");
+			System.out.println("invalid input");
 			
 		}
-		//System.out.println(digit1);
+		
 		System.out.println("Digit 2 :");
 		try
 		{
@@ -130,10 +134,10 @@ public class calc {
 		}
 		catch (InputMismatchException ex)
 		{	
-			System.out.println("wtf u entered m8");
+			System.out.println("invalid input");
 			
 		}
-		//System.out.println(digit2);
+		
 	}
 	
 	
@@ -141,9 +145,8 @@ public class calc {
 	
 
 	public static void main(String[] args) {
-		calc obj = new calc();
-		//obj.main(null);
-		obj.Menu();
+		calc obj = new calc(); // creating an object of class
+		obj.Menu(); // accessing menu that calling getDigits() Calculate()
 		
 	}
 
